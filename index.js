427b14842064c34fb09273ae68535a4296821185
Kanana-Mwenda <Lizamwenda95@gmail.com>
2024-12-02 15:45:06 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Kanana';
+
+ const welcomeMessage = "Welcome to Flatbook, $ 'Kanana!";
+
+ const excitedWelcomeMessage = "WELCOME TO FLATBOOK, $ 'KANANA'!";
+
+ const shortGreeting ="Welcome, 'K'!";
